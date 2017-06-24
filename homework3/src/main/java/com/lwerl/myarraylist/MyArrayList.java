4ea8ce3bc5aee10c1fb676331523bbb931b76fae
lWeRl <werasdas@gmail.com>
2017-06-24 10:20:01 +0300

    HW 3 fix constructor with intial size

--- homework3/src/main/java/com/lwerl/myarraylist/MyArrayList.java
+++ homework3/src/main/java/com/lwerl/myarraylist/MyArrayList.java
@@ -19,12 +19,12 @@ public class MyArrayList<E> implements List<E> {
     }
 
     public MyArrayList(int initSize) {
-        if (initSize > 0) {
-            array = new Object[initSize];
-        } else if (initSize == 0) {
+        if (initSize < 0) {
+            throw new IllegalArgumentException("Wrong initial size: " + initSize);
+        } else if (initSize <= 10) {
             array = new Object[DEFAULT_INITIAL_SIZE];
         } else {
-            throw new IllegalArgumentException("Wrong initial size: " + initSize);
+            array = new Object[initSize];
         }
     }
 
@@ -153,7 +153,7 @@ public class MyArrayList<E> implements List<E> {
         while (e1.hasNext() && e2.hasNext()) {
             E o1 = e1.next();
             Object o2 = e2.next();
-            if (!(o1==null ? o2==null : o1.equals(o2)))
+            if (!(o1 == null ? o2 == null : o1.equals(o2)))
                 return false;
         }
         return !(e1.hasNext() || e2.hasNext());
@@ -162,7 +162,7 @@ public class MyArrayList<E> implements List<E> {
     public int hashCode() {
         int hashCode = 1;
         for (E e : this)
-            hashCode = 31 * hashCode + (e==null ? 0 : e.hashCode());
+            hashCode = 31 * hashCode + (e == null ? 0 : e.hashCode());
         return hashCode;
     }
 
